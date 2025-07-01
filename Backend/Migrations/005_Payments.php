eab2338c7ab1fb4268c635f853b74f02b28d9ad2
Sino57 <sinarfayyad5@gmail.com>
2025-07-01 23:17:00 +0300

    Payments table

---
+++ Backend/Migrations/005_Payments.php
@@ -0,0 +1,18 @@
+<?php
+
+require("../Connection/connection.php");
+
+$query = "CREATE TABLE IF NOT EXISTS payments (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  user_name INT NOT NULL,
+  ticket_id INT NOT NULL,
+  amount DECIMAL(6,2) NOT NULL,
+  payment_method VARCHAR(50),
+  FOREIGN KEY (user_name) REFERENCES users(name),
+  FOREIGN KEY (ticket_id) REFERENCES tickets(id)
+)";
+
+$execute = $mysqli->prepare($query);
+$execute->execute();
+
+?>
