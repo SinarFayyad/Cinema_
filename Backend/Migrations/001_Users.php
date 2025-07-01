e34c197f9c34ddb63b2a25b34db6b373b9953642
Sino57 <sinarfayyad5@gmail.com>
2025-07-01 23:12:53 +0300

    Users table

---
+++ Backend/Migrations/001_Users.php
@@ -0,0 +1,17 @@
+<?php
+
+require("../Connection/connection.php");
+
+$query = "CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    age INT,
+    email VARCHAR(250) NOT NULL UNIQUE,
+    password VARCHAR(250) NOT NULL,
+    address VARCHAR(100) NOT NULL
+)";
+
+$execute = $mysqli->prepare($query);
+$execute->execute();
+
+?>
