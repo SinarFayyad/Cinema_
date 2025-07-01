ac564b3397940a223ae0a5b44c9993b2a24b4499
Sino57 <sinarfayyad5@gmail.com>
2025-07-02 00:24:46 +0300

    requires

---
+++ Backend/Controllers/requireAll.php
@@ -0,0 +1,18 @@
+<?php
+
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json");
+header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Allow-Headers: Content-Type");
+
+require("../Connection/connection.php");
+
+require("../Models/Admin.php");
+require("../Models/Model.php");
+require("../Models/Movie.php");
+require("../Models/Payment.php");
+require("../Models/Seat.php");
+require("../Models/Snack.php");
+require("../Models/Ticket.php");
+require("../Models/User.php");
+?>
