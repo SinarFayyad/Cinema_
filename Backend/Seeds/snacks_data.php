02ff7406d61b5d528085b15f1c99ffe424431d60
Sino57 <sinarfayyad5@gmail.com>
2025-07-07 01:26:41 +0300

    add snacks seeds

---
+++ Backend/Seeds/snacks_data.php
@@ -0,0 +1,16 @@
+<?php 
+
+require("../Models/Snack.php");
+require("../Models/Model.php");
+require("../Connection/connection.php");
+
+$snacks = [];
+
+$snacks[0] = ['name' => 'Popcorn', 'size' => 'Large', 'price' => 5.00, 'available' => true];
+$snacks[1] = ['name' => 'Nachos', 'size' => 'Medium', 'price' => 4.50, 'available' => true];
+$snacks[2] = ['name' => 'Soda', 'size' => 'Large', 'price' => 3.00, 'available' => true];
+$snacks[3] = ['name' => 'Candy', 'size' => 'Small', 'price' => 2.50, 'available' => true];
+
+foreach($snacks as $snack) {
+    Snack::create($mysqli, $snack);
+}
