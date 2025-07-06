adfb97af88f11bd051d3188e30a727396b0dc04c
Sino57 <sinarfayyad5@gmail.com>
2025-07-07 01:31:36 +0300

    more seeds

--- Backend/Seeds/payments_data.php
+++ Backend/Seeds/payments_data.php
@@ -5,3 +5,22 @@ require("../Models/Model.php");
 require("../Connection/connection.php");
 
 $payments = [];
+
+$payments[0] = ['user_name' => 'Nabiha Jaafar',
+                'ticket_id' => 1,
+                'amount' => 12.50,
+                'payment_method' => 'Credit Card'];
+
+$payments[1] = ['user_name' => 'Hassan Al-Mansour',
+                'ticket_id' => 2,
+                'amount' => 10.00,
+                'payment_method' => 'PayPal'];
+
+$payments[2] = ['user_name' => 'Layla Kassem',
+                'ticket_id' => 3,
+                'amount' => 12.50,
+                'payment_method' => 'Debit Card'];
+
+foreach ($payments as $payment) {
+    Payment::create($mysqli, $payment);
+}
---
+++ Backend/Seeds/seats_data.php
@@ -0,0 +1,15 @@
+<?php 
+
+require("../Models/Seat.php");
+require("../Models/Model.php");
+require("../Connection/connection.php");
+
+$seats = [];
+
+$seats[0] = ['row_number' => 1, 'column_number' => 1, 'available' => FALSE];
+$seats[1] = ['row_number' => 1, 'column_number' => 2, 'available' => FALSE];
+$seats[2] = ['row_number' => 2, 'column_number' =>1, 'available' => FALSE];
+
+foreach($seats as $seat) {
+    Seat::create($mysqli, $seat);
+}
