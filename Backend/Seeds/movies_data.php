31f828e4e4f9179a17d9695715ae92f7e3781e3e
Sino57 <sinarfayyad5@gmail.com>
2025-07-07 01:07:16 +0300

    add seeds

--- Backend/Seeds/movies_data.php
+++ Backend/Seeds/movies_data.php
@@ -6,10 +6,27 @@ require("../Connection/connection.php");
 
 $movies = [];
 
-$movies[0] = ['title' => 'Inception', 'director' => 'Christopher Nolan', 'release_year' => 2010, 'genre' => 'Science Fiction'];
-$movies[1] = ['title' => 'The Godfather', 'director' => 'Francis Ford Coppola', 'release_year' => 1972, 'genre' => 'Crime'];
-$movies[2] = ['title' => 'The Dark Knight', 'director' => 'Christopher Nolan', 'release_year' => 2008, 'genre' => 'Action'];
-$movies[3] = ['title' => 'Pulp Fiction', 'director' => 'Quentin Tarantino', 'release_year' => 1994, 'genre' => 'Crime'];
+$movies[0]= ['title' => 'Brave Citizen',
+             'genre' => 'Action', 
+             'release_year' => 2023, 
+             'screening_date' => '2023-10-01 19:00:00', 
+             'price' => 12.50, 
+             'location' => 'Cinema A', 
+             'poster_url' => 'https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcSzuYmaSOFfSNJgMrVcnJcUmJc0z-dvo5u9oTEnIwS8hrzozM9I5k3v6Bi-hFtWsv1zq82AsQ', 
+             'cast' => 'Lee Jun-young, Cha Woo-min, Shin Hye-sun, Park Jung-woo', 
+             'rating' => 6.5, 
+             'trailer_url' => 'https://youtu.be/1LinV73o7WM?si=SSJw9GJR4ANez6OX'];
+
+$movies[1] = ['title'=> 'Midnight',
+                'genre' => 'Thriller', 
+                'release_year' => 2023, 
+                'screening_date' => '2023-10-02 21:00:00', 
+                'price' => 10.00, 
+                'location' => 'Cinema B', 
+                'poster_url' => 'https://asianwiki.com/images/2/25/Midnight-KM-mp01.jpg',
+                'cast' => 'Wi Ha-Joon, Jin Ki-Joo, Jeong Won-Chang, Park Hoon', 
+                'rating' => 7.2, 
+                'trailer_url' => 'https://youtu.be/3Ap4ww9Ap-0?si=dxGwKn5et0LGzX7-'];
 
 foreach($movies as $movie) {
     Movie::create($mysqli, $movie);
---
+++ Backend/Seeds/users_data.php
@@ -0,0 +1,17 @@
+<?php 
+
+require("../Models/User.php");
+require("../Models/Model.php");
+require("../Connection/connection.php");
+
+$users = [];
+
+$users[0] = ['name' => 'Nabiha Jaafar', 'email' => 'nabihaJ@example.com', 'password' => password_hash('nabiha123', PASSWORD_DEFAULT)];
+$users[1] = ['name' => 'Hassan Al-Mansour', 'email' => 'hassnMansour@example.com', 'password' => password_hash('hassan123', PASSWORD_DEFAULT)];
+$users[2] = ['name' => 'Layla Kassem', 'email' => 'LaylaKassem@example.com', 'password' => password_hash('layla123', PASSWORD_DEFAULT)];
+$users[3] = ['name' => 'Omar Haddad', 'email' => 'omrHaddad@example.com', 'password' => password_hash('omar123', PASSWORD_DEFAULT)];
+$users[4] = ['name' => 'Fatima Al-Sayed', 'email' => 'fatimaSayed@example.com', 'password' => password_hash('fatima123', PASSWORD_DEFAULT)];
+
+foreach($users as $user) {
+    User::create($mysqli, $user);
+}
